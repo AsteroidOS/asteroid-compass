d54af8ee5e15c442252e12d9ac855f02d029176b
Darrel Griët <dgriet@gmail.com>
2022-07-10 02:07:13 +0200

    translations: Update translations.
    Signed-off-by: Darrel Griët <dgriet@gmail.com>

---
+++ i18n/asteroid-compass.ca.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca">
+<context>
+    <name></name>
+    <message id="id-no-data-calibrate">
+        <source>&lt;h3&gt;No data&lt;/h3&gt;Calibrate the sensor by moving it in an ∞ figure.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-app-launcher-name">
+        <source>Compass</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
