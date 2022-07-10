2d1397aee60776ccc7b07335b6b42f792761e360
Darrel Griët <dgriet@gmail.com>
2022-07-10 02:07:13 +0200

    translations: Update translations.
    Signed-off-by: Darrel Griët <dgriet@gmail.com>

---
+++ i18n/asteroid-compass.id.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="id">
+<context>
+    <name></name>
+    <message id="id-no-data-calibrate">
+        <location filename="../src/main.qml" line="45"/>
+        <source>&lt;h3&gt;No data&lt;/h3&gt;Calibrate the sensor by moving it in an ∞ figure.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-app-launcher-name">
+        <location filename="asteroid-compass.desktop.h" line="6"/>
+        <source>Compass</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ i18n/asteroid-compass.nl_NL.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name></name>
+    <message id="id-no-data-calibrate">
+        <location filename="../src/main.qml" line="45"/>
+        <source>&lt;h3&gt;No data&lt;/h3&gt;Calibrate the sensor by moving it in an ∞ figure.</source>
+        <translation>&lt;h3&gt;Geen data&lt;/h3&gt;Kalibreer de sensor door het in een ∞ figuur te bewegen.</translation>
+    </message>
+    <message id="id-app-launcher-name">
+        <location filename="asteroid-compass.desktop.h" line="6"/>
+        <source>Compass</source>
+        <translation>Compass</translation>
+    </message>
+</context>
+</TS>
