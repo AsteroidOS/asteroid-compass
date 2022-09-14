ac7ad857b6a2cfa5e0eec5bb37d2fa2d5be698b3
Milo Ivir <mail@milotype.de>
2022-09-14 15:53:51 +0000

    Translated using Weblate (Croatian)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: AsteroidOS/Compass
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/compass/hr/

--- i18n/asteroid-compass.hr.ts
+++ i18n/asteroid-compass.hr.ts
@@ -6,12 +6,12 @@
     <message id="id-no-data-calibrate">
         <location filename="../src/main.qml" line="45"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Calibrate the sensor by moving it in an ∞ figure.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;Nema podataka&lt;/h3&gt;Kalibriraj senzor pomicanjem senzora u ∞ znak.</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-compass.desktop.h" line="6"/>
         <source>Compass</source>
-        <translation type="unfinished"></translation>
+        <translation>Kompas</translation>
     </message>
 </context>
 </TS>
