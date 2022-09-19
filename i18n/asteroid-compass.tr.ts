fe8ad572f874c0a1853cbfcfce8c5651f94699bd
Oğuz Ersen <oguz@ersen.moe>
2022-09-19 19:09:58 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: AsteroidOS/Compass
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/compass/tr/

--- i18n/asteroid-compass.tr.ts
+++ i18n/asteroid-compass.tr.ts
@@ -5,11 +5,11 @@
     <name></name>
     <message id="id-no-data-calibrate">
         <source>&lt;h3&gt;No data&lt;/h3&gt;Calibrate the sensor by moving it in an ∞ figure.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;Veri yok&lt;/h3&gt;Sensörü ∞ şeklinde hareket ettirerek kalibre edin.</translation>
     </message>
     <message id="id-app-launcher-name">
         <source>Compass</source>
-        <translation type="unfinished"></translation>
+        <translation>Pusula</translation>
     </message>
 </context>
 </TS>
