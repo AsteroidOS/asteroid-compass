fbcd49e361595a78612fc67bec6b3e9e3ed2dc47
Prachi Joshi <josprachi@yahoo.com>
2022-09-25 09:14:51 +0000

    Translated using Weblate (Marathi)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: AsteroidOS/Compass
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/compass/mr/

--- i18n/asteroid-compass.mr.ts
+++ i18n/asteroid-compass.mr.ts
@@ -6,12 +6,12 @@
     <message id="id-no-data-calibrate">
         <location filename="../src/main.qml" line="45"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Calibrate the sensor by moving it in an ∞ figure.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;कोणताही डेटा नाही&lt;/h3&gt;सेन्सरला ∞ आकृतीमध्ये हलवून कॅलिब्रेट करा.</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-compass.desktop.h" line="6"/>
         <source>Compass</source>
-        <translation type="unfinished"></translation>
+        <translation>होकायंत्र</translation>
     </message>
 </context>
 </TS>
