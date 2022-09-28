8252eb866064dda261f563c0c951cc1a08f81250
Hoseok Seo <ddinghoya@gmail.com>
2022-09-28 08:36:07 +0000

    Translated using Weblate (Korean)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: AsteroidOS/Compass
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/compass/ko/

--- i18n/asteroid-compass.ko.ts
+++ i18n/asteroid-compass.ko.ts
@@ -6,12 +6,12 @@
     <message id="id-no-data-calibrate">
         <location filename="../src/main.qml" line="45"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Calibrate the sensor by moving it in an ∞ figure.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;데이터 없음&lt;/h3&gt;센서를 ∞ 피규어로 움직여 보정하세요.</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-compass.desktop.h" line="6"/>
         <source>Compass</source>
-        <translation type="unfinished"></translation>
+        <translation>컴퍼스</translation>
     </message>
 </context>
 </TS>
