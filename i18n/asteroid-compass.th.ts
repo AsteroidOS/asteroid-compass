c1e105c72572c02d3dab4cda26f6199b8cbc111f
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2022-10-05 19:33:11 +0000

    Translated using Weblate (Thai)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: AsteroidOS/Compass
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/compass/th/

--- i18n/asteroid-compass.th.ts
+++ i18n/asteroid-compass.th.ts
@@ -6,12 +6,12 @@
     <message id="id-no-data-calibrate">
         <location filename="../src/main.qml" line="45"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Calibrate the sensor by moving it in an ∞ figure.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;ไม่พบข้อมูล&lt;/h3&gt;ปรับเทียบเซ็นเซอร์ด้วยการขยับเซ็นเซอร์ในรูป ∞</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-compass.desktop.h" line="6"/>
         <source>Compass</source>
-        <translation type="unfinished"></translation>
+        <translation>เข็มทิศ</translation>
     </message>
 </context>
 </TS>
