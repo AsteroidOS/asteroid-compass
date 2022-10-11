bd925e653e07c3ccf57cc918067ce28f76b5e2e4
yangyangdaji <1504305527@qq.com>
2022-10-11 15:10:29 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: AsteroidOS/Compass
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/compass/zh_Hans/

--- i18n/asteroid-compass.zh_Hans.ts
+++ i18n/asteroid-compass.zh_Hans.ts
@@ -5,11 +5,11 @@
     <name></name>
     <message id="id-no-data-calibrate">
         <source>&lt;h3&gt;No data&lt;/h3&gt;Calibrate the sensor by moving it in an ∞ figure.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;无数据&lt;/h3&gt;以 ∞ 移动来校准传感器。</translation>
     </message>
     <message id="id-app-launcher-name">
         <source>Compass</source>
-        <translation type="unfinished"></translation>
+        <translation>罗盘</translation>
     </message>
 </context>
 </TS>
