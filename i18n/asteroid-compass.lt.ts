98965bc543862616085e3071d33776342a8310cf
Gediminas Murauskas <muziejusinfo@gmail.com>
2022-10-15 16:58:10 +0000

    Translated using Weblate (Lithuanian)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: AsteroidOS/Compass
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/compass/lt/

--- i18n/asteroid-compass.lt.ts
+++ i18n/asteroid-compass.lt.ts
@@ -6,12 +6,12 @@
     <message id="id-no-data-calibrate">
         <location filename="../src/main.qml" line="45"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Calibrate the sensor by moving it in an ∞ figure.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;Nėra duomenų&lt;/h3&gt;Kalibruokite jutiklį judindami jį ∞ figūra.</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-compass.desktop.h" line="6"/>
         <source>Compass</source>
-        <translation type="unfinished"></translation>
+        <translation>Kompasas</translation>
     </message>
 </context>
 </TS>
