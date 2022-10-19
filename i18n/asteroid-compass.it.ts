f6cfc63267a15b3c126f162a401abc8a17f9f8fe
A B <alessandrobrugnera16@protonmail.com>
2022-10-19 21:28:06 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: AsteroidOS/Compass
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/compass/it/

--- i18n/asteroid-compass.it.ts
+++ i18n/asteroid-compass.it.ts
@@ -5,11 +5,11 @@
     <name></name>
     <message id="id-no-data-calibrate">
         <source>&lt;h3&gt;No data&lt;/h3&gt;Calibrate the sensor by moving it in an ∞ figure.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;Nessun dato&lt;/h3&gt;Calibra il sensore muovendolo, disegnando un ∞.</translation>
     </message>
     <message id="id-app-launcher-name">
         <source>Compass</source>
-        <translation type="unfinished"></translation>
+        <translation>Bussola</translation>
     </message>
 </context>
 </TS>
