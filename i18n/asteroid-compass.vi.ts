c976cb3c0ce60fc84f0c5c4a862bed3ede698c2d
Minh P <phandinhminh@protonmail.ch>
2022-11-25 23:33:35 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: AsteroidOS/Compass
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/compass/vi/

--- i18n/asteroid-compass.vi.ts
+++ i18n/asteroid-compass.vi.ts
@@ -6,12 +6,12 @@
     <message id="id-no-data-calibrate">
         <location filename="../src/main.qml" line="45"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Calibrate the sensor by moving it in an ∞ figure.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;Không dữ liệu&lt;/h3&gt;Hiệu chỉnh cảm biến bằng cách di chuyển nó theo hình ∞.</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-compass.desktop.h" line="6"/>
         <source>Compass</source>
-        <translation type="unfinished"></translation>
+        <translation>La bàn</translation>
     </message>
 </context>
 </TS>
