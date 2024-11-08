490ece8ef0481e777e8f444392fb4e62b77dcf95
தமிழ்நேரம் <anishprabu.t@gmail.com>
2024-11-08 12:49:05 +0000

    Translated using Weblate (Tamil)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: AsteroidOS/Compass
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/compass/ta/

--- i18n/asteroid-compass.ta.ts
+++ i18n/asteroid-compass.ta.ts
@@ -5,11 +5,11 @@
     <name></name>
     <message id="id-no-data-calibrate">
         <source>&lt;h3&gt;No data&lt;/h3&gt;Calibrate the sensor by moving it in an ∞ figure.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;H3&gt; தரவு இல்லை &lt;/H3&gt; சென்சாரை ஒரு ∞ படத்தில் நகர்த்துவதன் மூலம் அதை அளவீடு செய்யுங்கள்.</translation>
     </message>
     <message id="id-app-launcher-name">
         <source>Compass</source>
-        <translation type="unfinished"></translation>
+        <translation>திசைகாட்டி</translation>
     </message>
 </context>
 </TS>
