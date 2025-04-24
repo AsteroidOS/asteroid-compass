f0fc032700e062a77d69b191abbe93cd0e497961
GiannosOB <giannos2105@gmail.com>
2025-04-24 12:52:59 +0200

    Translated using Weblate (Greek)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: AsteroidOS/Compass
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/compass/el/

--- i18n/asteroid-compass.el.ts
+++ i18n/asteroid-compass.el.ts
@@ -5,11 +5,11 @@
     <name></name>
     <message id="id-no-data-calibrate">
         <source>&lt;h3&gt;No data&lt;/h3&gt;Calibrate the sensor by moving it in an ∞ figure.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;Δεν υπάρχουν δεδομένα&lt;/h3&gt;Βαθμονομήστε τον αισθητήρα μετακινώντας τον σε σχήμα ∞.</translation>
     </message>
     <message id="id-app-launcher-name">
         <source>Compass</source>
-        <translation type="unfinished"></translation>
+        <translation>Πυξίδα</translation>
     </message>
 </context>
 </TS>
