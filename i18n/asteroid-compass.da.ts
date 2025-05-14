bbbda6efb250fa386e16ba6c03c2f4c67f68f900
Jacob Madsen <jamesoft@gmail.com>
2025-05-14 09:28:24 +0200

    Translated using Weblate (Danish)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: AsteroidOS/Compass
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/compass/da/

--- i18n/asteroid-compass.da.ts
+++ i18n/asteroid-compass.da.ts
@@ -6,12 +6,12 @@
     <message id="id-no-data-calibrate">
         <location filename="../src/main.qml" line="45"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Calibrate the sensor by moving it in an ∞ figure.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;Ingen data&lt;/h3&gt;Kalibrer sensoren ved at flytte den i en ∞-figur.</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-compass.desktop.h" line="6"/>
         <source>Compass</source>
-        <translation type="unfinished"></translation>
+        <translation>Kompas</translation>
     </message>
 </context>
 </TS>
