cd333916b71b00d91f9cfc3393799ee4277ff301
Adolfo Jayme Barrientos <fitojb@ubuntu.com>
2025-06-01 18:40:51 +0200

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: AsteroidOS/Compass
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/compass/es/

--- i18n/asteroid-compass.es.ts
+++ i18n/asteroid-compass.es.ts
@@ -6,7 +6,7 @@
     <message id="id-no-data-calibrate">
         <location filename="../src/main.qml" line="45"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Calibrate the sensor by moving it in an ∞ figure.</source>
-        <translation>&lt;h3&gt;Sin datos&lt;/h3&gt;Calibra el sensor moviéndolo haciendo una figura ∞.</translation>
+        <translation>&lt;h3&gt;Sin datos&lt;/h3&gt;Calibre el sensor moviéndolo haciendo un ∞.</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-compass.desktop.h" line="6"/>
