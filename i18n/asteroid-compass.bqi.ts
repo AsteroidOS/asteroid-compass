56fa40a1740416483885c9664cf1b43c35b56bef
hosêyň abāspanā <hosseinabaspanah@gmail.com>
2025-06-30 06:38:12 +0200

    Translated using Weblate (Luri (Bakhtiari))
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: AsteroidOS/Compass
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/compass/bqi/

--- i18n/asteroid-compass.bqi.ts
+++ i18n/asteroid-compass.bqi.ts
@@ -6,12 +6,12 @@
     <message id="id-no-data-calibrate">
         <location filename="../src/main.qml" line="45"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Calibrate the sensor by moving it in an ∞ figure.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;بؽ داده&lt;/h3&gt;کالیبره هسگر وا لر داڌنس و جۊر ∞.</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-compass.desktop.h" line="6"/>
         <source>Compass</source>
-        <translation type="unfinished"></translation>
+        <translation>قوتب نما</translation>
     </message>
 </context>
 </TS>
