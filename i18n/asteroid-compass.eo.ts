283f24430526f1c905df9fa8206e547daf44e95b
Valentin Ljuba <7fnndt50@anonaddy.me>
2025-07-07 20:02:43 +0200

    Translated using Weblate (Esperanto)
    Currently translated at 0.0% (0 of 2 strings)
    
    Translation: AsteroidOS/Compass
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/compass/eo/

--- i18n/asteroid-compass.eo.ts
+++ i18n/asteroid-compass.eo.ts
@@ -6,7 +6,7 @@
     <message id="id-no-data-calibrate">
         <location filename="../src/main.qml" line="45"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Calibrate the sensor by moving it in an ∞ figure.</source>
-        <translation type="unfinished">&lt;h3&gt;Žádná data&lt;/h3&gt;Kalibrujte senzor pomocí pohybu v ∞ obrazci.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-compass.desktop.h" line="6"/>
