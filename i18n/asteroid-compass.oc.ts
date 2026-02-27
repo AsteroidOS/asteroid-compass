65bd5d49d41b3f55f001da108e39e79e613ad022
Quentin PAGÈS <quentinantonin@free.fr>
2026-02-27 08:45:39 +0100

    Translated using Weblate (Occitan)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: AsteroidOS/Compass
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/compass/oc/

--- i18n/asteroid-compass.oc.ts
+++ i18n/asteroid-compass.oc.ts
@@ -6,7 +6,7 @@
     <message id="id-no-data-calibrate">
         <location filename="../src/main.qml" line="45"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Calibrate the sensor by moving it in an ∞ figure.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;Cap de donadas&lt;/h3&gt;Calibratz lo sensor en lo movent en fòrma d’∞.</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-compass.desktop.h" line="6"/>
